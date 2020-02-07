48dcc32f80314f312398e0be887db2538c8cb1fd
Tim De Paepe <tim.depaepe@gmail.com>
2020-02-07 01:02:28 +0100

    initial updates

--- app/js/app.js
+++ app/js/app.js
@@ -11,52 +11,159 @@ App = {
   * This function stays on top, it wil initialize the app
   */
  init: () => {
-    // set the title of the document
-    App.setDocumentTitle(`${data.general.firstName} ${data.general.lastName} | ${data.general.class} | Artevelde University College Ghent`);
+   // the assignment numbers
+   const assignmentNumbers = [ 1, 2, 3, 4, 5 ];
 
-    App.addLink({
-       parent: document.getElementById('assignment1'),
-       text: 'dit is een test',
-       href: data.assignment1.pdfUrl,
-       title: 'ttttt',
-       className: 'myClass'
+   // create the users name
+   const name = `${data.general.firstName} ${data.general.lastName}`;
+   const group = `${data.general.class}`;
+
+   // set the title of the document
+   AppView.setDocumentTitle(`${name} | ${group} | Artevelde University College Ghent`);
+   document.querySelector('.heading-name').innerHTML = `${name}, ${group}`;
+
+   // create the buttons for each assignment and add them to the DOM
+   assignmentNumbers.forEach((number) => {
+    AppView.appendButtonsForAssignment(
+      document.querySelector(`.assignment${number} .work-buttons-container`),
+      data[`assignment${number}`].buttons)
+   });
+
+   // init the mobile navigation
+   App.initMobileNavigation();
+
+   // init the scrolling behaviour
+   App.initScrolling();
+ },
+
+ initMobileNavigation: () => {
+   // get the mobile menu
+   const mobileMenu = document.querySelector('.mobile-menu');
+
+   // set the event handler
+   mobileMenu.addEventListener('click', () => {
+      if(document.querySelector('.header-menu').classList.contains('open')) AppView.closeMobileNavigation();
+      else AppView.openMobileNavgation();
+   });
+ },
+
+
+ initScrolling: () => {
+    document.querySelectorAll('.nav-link').forEach((element) => {
+      // get the element to scroll to
+      const navigateTo = element.dataset.navigateto;
+
+      // validate
+      if(typeof(navigateTo) === "undefined") return;
+
+      // settings
+      const behavior = "smooth";
+      const block = "center";
+      const inline = "nearest";
+
+      // scroll to the container if we click the link
+      element.addEventListener('click', (e) => {
+         // preven default behaviour
+         e.preventDefault();
+
+         // close mobile view if needed
+         if(document.querySelector('.header-menu').classList.contains('open')) AppView.closeMobileNavigation();
+
+         // scroll to position
+         document.querySelector(`.${navigateTo}`).scrollIntoView({ behavior, block, inline });
+      });
     });
- },
+ }
 
- /**
-  * This will check if an object is an HTML5 element
-  * source: https://stackoverflow.com/questions/4754088/how-to-check-if-object-is-a-dom-element/4754104
-  */
- isElement: (obj) => obj instanceof Element,
+}
 
- /**
-  * Adds an anchor element to a given parameter
-  */
- addLink: ({ parent, text, href, title, className, target='_self' }) => {
-    // check incoming variables
-    if(!App.isElement(parent)) return;
+/**
+ * The logic for the view for the app
+ */
+AppView = {
 
-    // create a new anchor element
-    const a = document.createElement('a');
+  /**
+   * Sets the title of the HTML document
+   */
+   setDocumentTitle: (title) => document.title = title,
 
-    // set the text to click on and add this to the anchor element
-    var linkText = document.createTextNode(text);
-    a.appendChild(linkText);
+   /**
+    * Open the mobile navigation menu
+    */
+   openMobileNavgation: () => {
+      document.querySelector('.app').classList.add('fixed');
+      document.querySelector('.mobile-menu').classList.add('mobile');
+      document.querySelector('.header-menu').classList.add('open');
+   },
 
-    // set some anchor properties
-    if(href) a.href = href;
-    if(className) a.className = className;
-    if(title) a.title = title;
-    if(target) a.target = target;
+   /**
+    * Close the mobile navigation menu
+    */
+   closeMobileNavigation: () => {
+      document.querySelector('.app').classList.remove('fixed');
+      document.querySelector('.mobile-menu').classList.remove('mobile');
+      document.querySelector('.header-menu').classList.remove('open');
+   },
 
-    // append the anchor to our given parent element
-    parent.appendChild(a);
- },
+   /**
+    * Add buttons (given in JSON { text, title, href} to an assignment container)
+    */
+   appendButtonsForAssignment: ((parent, buttons) => {
+      // validate the incoming variables
+      if(typeof(buttons) === "undefined" || buttons.length <= 0) return;
 
- /**
-  * Sets the title of the HTML document
-  */
- setDocumentTitle: (title) => document.title = title
+      // loop over the buttons data and add them to the view
+      buttons.forEach((button) => {
+         // get the data, safely
+         const text = typeof(button.text) === "undefined" ? "" : button.text;
+         const title = typeof(button.title) === "undefined" ? "" : button.title;
+         const href = typeof(button.href) === "undefined" ? "" : button.href;
+         const target = typeof(button.text) === "undefined" ? "_self" : button.target;
+
+         // append the button
+         AppView.appendButton({ parent, text, title, href, target });
+      });
+   }),
+
+   /**
+    * Render a button
+    */
+   appendButton: ({ parent, text, href, title, target='_self' }) => {
+       // check incoming variables
+       if(!AppTools.isElement(parent)) return;
+
+       // create a new anchor element
+       const a = document.createElement('a');
+
+       // set the text to click on and add this to the anchor element
+       var linkText = document.createTextNode(text);
+       a.appendChild(linkText);
+
+       // set the button class
+       a.className = 'button';
+
+       // set some anchor properties
+       if(href) a.href = href;
+       if(title) a.title = title;
+       if(target) a.target = target;
+
+       // append the anchor to our given parent element
+       parent.appendChild(a);
+   }
+
+}
+
+/**
+ * Some tools to make life more comfortable
+ */
+AppTools = {
+
+  /**
+   * This will check if an object is an HTML5 element
+   * source: https://stackoverflow.com/questions/4754088/how-to-check-if-object-is-a-dom-element/4754104
+   */
+   isElement: (obj) => obj instanceof Element,
+
 }
 
 // when window is loaded, init the app
--- data.js
+++ data.js
@@ -1,26 +1,141 @@
 /**
  * Please edit the necessary data to complete your portfolio
+ * Change the assignment URLs (href) if needed
  */
 
 const data = {
+
+    /**
+     * Change the general data here (this one is used to find out who you are)
+     */
+
     "general": {
-        "firstName": "Tim",
-        "lastName": "De Paepe",
-        "class": "1CMOe"
+        "firstName": "Andrej",      // fill in with your own first name
+        "lastName": "Tarkovski",    // fill in with your own last name
+        "class": "1CMOe"            // fill in with your class/group e.g. 1AVDa, 1CMOa, 1NMD, ...
     },
+
+    /**
+     * Assignment 1
+     */
+
     "assignment1": {
-        "pdfUrl": "./assignments/assignment1/test.pdf"
+        "buttons": [
+            {
+                "text": "Inspiratieboek",
+                "title": "Mijn PDF met inspiratie (login, functions, result)",
+                "href": "./assignments/assignment1/documents/a1-inspiration.pdf",
+                "target": "_blank"
+            },
+            {
+                "text": "Adobe XD",
+                "title": "Mijn Adobe XD document",
+                "href": "./assignments/assignment1/documents/a1-visual-designs.xd",
+            },
+            {
+                "text": "Adobe XD Prototype",
+                "title": "Mijn Adobe XD prototype (share link)",
+                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "target": "_blank"
+            }
+        ]
     },
+
+    /**
+     * Assignment 2
+     */
+
     "assignment2": {
-
+        "buttons": [
+            {
+                "text": "Inspiratieboek",
+                "title": "Mijn PDF met inspiratie",
+                "href": "./assignments/assignment2/documents/a2-inspiration.pdf",
+                "target": "_blank"
+            },
+            {
+                "text": "Adobe XD",
+                "title": "Mijn Adobe XD document",
+                "href": "./assignments/assignment2/documents/a2-visual-designs.xd",
+            },
+            {
+                "text": "Adobe XD Prototype",
+                "title": "Mijn Adobe XD prototype (share link)",
+                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "target": "_blank"
+            }
+        ]
     },
+
+    /**
+     * Assignment 3
+     */
+
     "assignment3": {
-
+        "buttons": [
+            {
+                "text": "Adobe XD",
+                "title": "Mijn Adobe XD document met Wireframes & Visual Design",
+                "href": "./assignments/assignment3/documents/a3-wireframes-visual-designs.xd",
+            },
+            {
+                "text": "Adobe XD Prototype",
+                "title": "Mijn Adobe XD prototype (share link)",
+                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "target": "_blank"
+            },
+            {
+                "text": "Website",
+                "title": "Mijn website voor opdracht 3",
+                "href": "./assignments/assignment3/website/index.html",
+                "target": "_blank"
+            }
+        ]
     },
+
+    /**
+     * Assignment 4
+     */
+
     "assignment4": {
-
+        "buttons": [
+            {
+                "text": "Analyse",
+                "title": "Mijn PDF met een analyse van de gemaakte fouten",
+                "href": "./assignments/assignment4/documents/a4-analysis.pdf",
+                "target": "_blank"
+            },
+            {
+                "text": "Adobe XD (Desktop)",
+                "title": "Mijn Adobe XD document voor de Desktop versie van de applicatie",
+                "href": "./assignments/assignment4/documents/a4-visual-designs-desktop.xd",
+            },
+            {
+                "text": "Adobe XD (Mobile)",
+                "title": "Mijn Adobe XD document voor de Mobile versie van de applicatie",
+                "href": "./assignments/assignment4/documents/a4-visual-designs-mobile.xd",
+            },
+            {
+                "text": "Website",
+                "title": "Mijn website voor opdracht 4",
+                "href": "./assignments/assignment4/website/index.html",
+                "target": "_blank"
+            }
+        ]
     },
-    "assignment5": {
 
+    /**
+     * Assignment 5
+     */
+
+    "assignment5": {
+        "buttons": [
+            {
+                "text": "Website",
+                "title": "Mijn website voor opdracht 5",
+                "href": "./assignments/assignment5/website/index.html",
+                "target": "_blank"
+            }
+        ]
     }
 }
